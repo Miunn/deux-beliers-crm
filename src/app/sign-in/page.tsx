d6c9514a67df60a174898ad4c2ad289da73fb13e
Miunn <remi@remicaulier.fr>
2025-10-07 22:25:18 +0200

    fix(page): overflow hidden

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function SignInPage() {
   return (
-    <div className="flex h-screen items-center justify-center overflow-hidden">
+    <div className="relative flex h-screen items-center justify-center overflow-hidden">
       <Image
         className="absolute top-0 left-0 opacity-10"
         src="/cropped-icon-gold.svg"
