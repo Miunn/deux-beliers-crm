57286b9b8961e2ff5625c6c78f066a816852e08b
Rémi Caulier <remi@remicaulier.fr>
2025-12-09 13:51:49 +0100

    feat(reminder): update reminder date within event, doesn't scroll on reminded event, name on events dialog

--- src/app/api/contacts/[id]/events/route.ts
+++ src/app/api/contacts/[id]/events/route.ts
@@ -1,15 +1,17 @@
 import { EventsService } from "@/data/events-service";
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/lib/auth";
+import { ContactService } from "@/data/contact-service";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: Promise<{ id: string }> }
+  { params }: { params: Promise<{ id: string }> },
 ) {
   const session = await auth.api.getSession({ headers: req.headers });
   if (!session)
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   const { id } = await params;
   const events = await EventsService.getByContact(id);
-  return NextResponse.json(events, { status: 200 });
+  const contact = await ContactService.getById(id);
+  return NextResponse.json({ contact, events }, { status: 200 });
 }
